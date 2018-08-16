e2859b427c5549067c449ec20ba2b5e0163ed4ed
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-08-16 16:23:54 +0800

    rename CACHE to MEMORY
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 
 DEFAULT_EXPIRE_TIME="7d"
 DEFAULT_JPG_QUALITY="95"
-DEFAULT_CACHE_SZIE="2048m"
+DEFAULT_MEMORY_SIZE="20m"
 
 [ -z "$NGINX_HOST" ] && echo "missing NGINX_HOST variable" && exit 1
 [ -z "$IMAGE_HOST" ] && echo "missing IMAGE_HOST variable" && exit 1
 [ -n "$EXPIRE_TIME" ] && DEFAULT_EXPIRE_TIME=$EXPIRE_TIME
 [ -n "$JPG_QUALITY" ] && DEFAULT_JPG_QUALITY=$JPG_QUALITY
-[ -n "$CACHE_SIZE" ] && DEFAULT_CACHE_SZIE=$CACHE_SIZE
+[ -n "$MEMORY_SIZE" ] && DEFAULT_MEMORY_SZIE=$MEMORY_SIZE
 
 IMAGE_HOST=${IMAGE_HOST//\//\\/}
 sed -i -e "s/\${NGINX_HOST}/$NGINX_HOST/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${IMAGE_HOST}/$IMAGE_HOST/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${EXPIRE_TIME}/$DEFAULT_EXPIRE_TIME/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${JPG_QUALITY}/$DEFAULT_JPG_QUALITY/" /etc/nginx/conf.d/default.conf
-sed -i -e "s/\${CACHE_SIZE}/$DEFAULT_CACHE_SZIE/" /etc/nginx/conf.d/default.conf
+sed -i -e "s/\${MEMORY_SIZE}/$DEFAULT_MEMORY_SZIE/" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
