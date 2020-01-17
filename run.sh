11633dfd432fd58cd6a922f3d0813f66854632ff
Oleg Nosov <nosovoleg@gmail.com>
2020-01-17 19:16:10 +0200

    fix typo

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ DEFAULT_INACTIVE_TIME="30d"
 [ -z "$IMAGE_HOST" ] && echo "missing IMAGE_HOST variable" && exit 1
 [ -n "$EXPIRE_TIME" ] && DEFAULT_EXPIRE_TIME=$EXPIRE_TIME
 [ -n "$JPG_QUALITY" ] && DEFAULT_JPG_QUALITY=$JPG_QUALITY
-[ -n "$MEMORY_SIZE" ] && DEFAULT_MEMORY_SZIE=$MEMORY_SIZE
+[ -n "$MEMORY_SIZE" ] && DEFAULT_MEMORY_SIZE=$MEMORY_SIZE
 [ -n "$CACHE_NAME" ] && DEFAULT_CACHE_NAME=$CACHE_NAME
 [ -n "$CACHE_SIZE" ] && DEFAULT_CACHE_SIZE=$CACHE_SIZE
 [ -n "$INACTIVE_TIME" ] && DEFAULT_INACTIVE_TIME=$INACTIVE_TIME
@@ -21,7 +21,7 @@ sed -i -e "s/\${NGINX_HOST}/$NGINX_HOST/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${IMAGE_HOST}/$IMAGE_HOST/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${EXPIRE_TIME}/$DEFAULT_EXPIRE_TIME/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${JPG_QUALITY}/$DEFAULT_JPG_QUALITY/" /etc/nginx/conf.d/default.conf
-sed -i -e "s/\${MEMORY_SIZE}/$DEFAULT_MEMORY_SZIE/" /etc/nginx/conf.d/default.conf
+sed -i -e "s/\${MEMORY_SIZE}/$DEFAULT_MEMORY_SIZE/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${CACHE_NAME}/$DEFAULT_CACHE_NAME/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${CACHE_SIZE}/$DEFAULT_CACHE_SIZE/" /etc/nginx/conf.d/default.conf
 sed -i -e "s/\${INACTIVE_TIME}/$DEFAULT_INACTIVE_TIME/" /etc/nginx/conf.d/default.conf
